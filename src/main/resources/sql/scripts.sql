e30d8408e3292cc28c38b4cc557ccc9cf1fcee34
Subhrajyoti Samanta <subhrajyoti.mark@gmail.com>
2025-04-08 00:32:17 +0530

    Updated Push

---
+++ src/main/resources/sql/scripts.sql
@@ -0,0 +1,20 @@
+create table users(username varchar_ignorecase(50) not null primary key,password varchar_ignorecase(500) not null,enabled boolean not null);
+create table authorities (username varchar_ignorecase(50) not null,authority varchar_ignorecase(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
+create unique index ix_auth_username on authorities (username,authority);
+
+INSERT IGNORE INTO `users` VALUES ('user', '{noop}SecureBank@12345', '1');
+INSERT IGNORE INTO `authorities` VALUES ('user', 'read');
+
+INSERT IGNORE INTO `users` VALUES ('admin', '{bcrypt}$2a$12$aFa0Uq0hreA8LadSA8VpPuRfu2n.ywW9fA1mbJ3BjF43PUH5Fj9Za', '1');
+INSERT IGNORE INTO `authorities` VALUES ('admin', 'admin');
+
+CREATE TABLE `customer` (
+                            `id` int NOT NULL AUTO_INCREMENT,
+                            `email` varchar(45) NOT NULL,
+                            `pwd` varchar(200) NOT NULL,
+                            `role` varchar(45) NOT NULL,
+                            PRIMARY KEY (`id`)
+);
+
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('happy@example.com', '{noop}SecureBank@12345', 'read');
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('admin@example.com', '{bcrypt}$2a$12$aFa0Uq0hreA8LadSA8VpPuRfu2n.ywW9fA1mbJ3BjF43PUH5Fj9Za', 'admin');
