3f8463aaacc86d0d17792d18556ca4952f1aee23
Subhrajyoti Samanta <subhrajyoti.mark@gmail.com>
2025-04-12 00:55:46 +0530

    Updated Push

---
+++ src/main/java/com/spring/security/entity/Customer.java
@@ -0,0 +1,23 @@
+package com.spring.security.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+
+    private String pwd;
+
+    private String role;
+
+}
