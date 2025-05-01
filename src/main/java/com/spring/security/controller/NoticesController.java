aaee6ff579612aca1d432d010fd31bd8ec3f514e
Subhrajyoti Samanta <Subhrajyoti.mark@gmail.com>
2025-05-01 22:37:35 +0530

    Updated Commit wih latest Pom

--- src/main/java/com/spring/security/controller/NoticesController.java
+++ src/main/java/com/spring/security/controller/NoticesController.java
@@ -5,6 +5,7 @@ import com.spring.security.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
